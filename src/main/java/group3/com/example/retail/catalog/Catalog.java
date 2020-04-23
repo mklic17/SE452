d64b2bf3ad6502e84e1a5de23ad0c3ec39ea5339
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2020-04-23 19:33:01 -0400

    added flywaydb init and createData scripts

--- src/main/java/group3/com/example/retail/catalog/Catalog.java
+++ src/main/java/group3/com/example/retail/catalog/Catalog.java
@@ -1,12 +1,22 @@
 package group3.com.example.retail.catalog;
 
+import java.io.Serializable;
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.JoinColumn;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Table;
+
+import group3.com.example.retail.product.Product;
+
 import javax.persistence.OneToMany;
 import lombok.Data;
 import lombok.ToString;
@@ -25,11 +35,14 @@ public class Catalog {
 	@Column(name="name")
 	private String _name;
 	
-	// LIST
+
+	 @ManyToMany
+    Set<Product> _productAssignments;
+
 	
-	public Catalog(String name) {
-		_name = name;
-	}
-	
+//	public Catalog(String name) {
+//		_name = name;
+//	}
+//	
 	
 }
--- src/main/java/group3/com/example/retail/product/Product.java
+++ src/main/java/group3/com/example/retail/product/Product.java
@@ -1,13 +1,22 @@
 package group3.com.example.retail.product;
 
+import java.io.Serializable;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.ManyToMany;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
 
 import lombok.Data;
+
+import group3.com.example.retail.catalog.Catalog;
+
 
 @Data
 @Entity
@@ -26,6 +35,15 @@ public class Product {
     
     @Column(name="price")
     private double _price;
+    
+    @ManyToMany
+    @JoinTable(
+    		name="ProductsInCategory",
+    		joinColumns=@JoinColumn(name="productId"),
+    		inverseJoinColumns=@JoinColumn(name="categoryId")
+    )
+    private Set<Catalog> _catalogAssignments;
+	
 	
 	
 //	public Product(String name, String description, double price) {
