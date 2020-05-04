166c50e39b8bee3e53232b22fffe6d752c80a3d8
brianobmalay <nikadas157@comcast.net>
2020-05-04 17:46:59 -0500

    fixed commandline

--- src/main/java/group3/com/example/retail/RetailApplication.java
+++ src/main/java/group3/com/example/retail/RetailApplication.java
@@ -2,11 +2,8 @@ package group3.com.example.retail;
 
 
 import java.sql.Timestamp;
-import java.util.List;
 
 import group3.com.example.retail.cart.*;
-import group3.com.example.retail.catalog.Catalog;
-import group3.com.example.retail.product.Product;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -38,7 +35,7 @@ public class RetailApplication {
 	}
 	
 	 @Bean
-	    public CommandLineRunner returnItem(CartRepository repository, OrdersRepo orderrepo) {
+	    public CommandLineRunner returnItem(RefundRepository repository, OrdersRepo orderrepo) {
 	        return (args) -> {
 	            // save a review
 	        	
