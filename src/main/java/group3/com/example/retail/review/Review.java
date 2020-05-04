5f7ed53eb95a4b95626119de38c2868d8883cc69
RickeySerna <49536586+RickeySerna@users.noreply.github.com>
2020-05-04 15:09:38 -0500

    Update Review.java

--- src/main/java/group3/com/example/retail/review/Review.java
+++ src/main/java/group3/com/example/retail/review/Review.java
@@ -1,5 +1,28 @@
 package group3.com.example.retail.review;
 
+
+import lombok.Data;
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name="Review")
 public class Review {
+  
+  // not done, how it will basically go
+  int[] totalRatings; // where all ratings for this product will be pulled from DB, may or may not end up being an array
+  public int getAverageRating(int[] ratings) {
+    
+    int average;
+    for (int i = 0; i < ratings.length; i++) {
+      average+=ratings[i];
+    }
+    
+    average = average/ratings.length;
+    
+    return average;
+  }
+  
 
 }
