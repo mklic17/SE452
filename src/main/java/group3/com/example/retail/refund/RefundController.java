b52c06e63b170fdbd740a76d0910200279d4860e
brianobmalay <nikadas157@comcast.net>
2020-05-17 15:44:07 -0500

    refund stuff

--- src/main/java/group3/com/example/retail/refund/RefundController.java
+++ src/main/java/group3/com/example/retail/refund/RefundController.java
@@ -19,45 +19,7 @@ import group3.com.example.retail.product.Product;
 public class RefundController {
 	
 	
-//	@RequestMapping("/refund/{Id}")
-//	public ModelAndView getRefundPage(@PathVariable Long Id) {
-//		ModelAndView mnv = new ModelAndView();
-//		mnv.setViewName("refund/form");
-////		mnv.setObject("Order", getOrder(Id));
-//		return mnv;
-//	}
-
-	@RequestMapping("/product/refund")
-	 public String newProduct(Model model){
-        model.addAttribute("refundForm", new RefundForm());
-        return "product/refundform";
-    }
-	
-	
-	@GetMapping("/")
-	public ModelAndView getRefundPage() {
-		ModelAndView mnv = new ModelAndView();
-		mnv.setViewName("review/form");
-		mnv.addObject("Refund", new Refund());
-		return mnv;
-	}
-	
-	@PostMapping("/createRefund")
-	public ModelAndView submit(@Valid Refund ref, BindingResult result) {
-		ModelAndView mnv = new ModelAndView();
-//	    if(result.hasErrors()) {
-//			mnv.setViewName("refund/form");
-//	        mnv.addObject("refund", ref);
-//	        return mnv;
-//	    }	
-		mnv.setViewName("other/home");
-
-//	    productService.updateProduct(prod.getId(), prod);
-	    // refundService.update()
-	    return mnv;
-	}
 
 	
 
 }
-//
