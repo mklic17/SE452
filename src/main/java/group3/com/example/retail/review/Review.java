3b7aa22aac83ad0022517c1ab4dcb8741fb57419
brianobmalay <nikadas157@comcast.net>
2020-05-17 16:06:18 -0500

    dfdf

--- src/main/java/group3/com/example/retail/review/Review.java
+++ src/main/java/group3/com/example/retail/review/Review.java
@@ -12,17 +12,7 @@ public class Review {
   
   // not done, how it will basically go
   int[] totalRatings; // where all ratings for this product will be pulled from DB, may or may not end up being an array
-  public int getAverageRating(int[] ratings) {
-    
-    int average;
-    for (int i = 0; i < ratings.length; i++) {
-      average+=ratings[i];
-    }
-    
-    average = average/ratings.length;
-    
-    return average;
-  }
+  
   
 
 }
